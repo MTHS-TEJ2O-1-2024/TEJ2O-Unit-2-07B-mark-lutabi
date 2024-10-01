42fd383ee02eda9cd37fd2264b61b0eade6b5243
mark-lutabi <mark.lutabi@mths.ca>
2024-10-01 11:16:08 -0400

    cookie monster

--- main.ts
+++ main.ts
@@ -1,8 +1,24 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Mark
+ * Created on: Sep 2024
+ * This program adds cookies when the switch is pressed
 */
 
-basic.showString('Hello, World!')
+// adds cookies
+
+let cookieNumber: number
+cookieNumber = 0
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function () {
+    cookieNumber = (cookieNumber + 1)
+    basic.showNumber(cookieNumber)
+})
+
+// resets variable to 0
+input.onButtonPressed(Button.B, function () {
+    cookieNumber = 0
+    basic.showString(cookieNumber.toString())
+})
